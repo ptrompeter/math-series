db632aa58769858928ba616e2cfdb1affb533b19
ptrompeter <patrick.trompeter@gmail.com>
2016-03-01 14:35:17 -0800

    series now works for fib where fib(1) = 0

--- src/series.py
+++ src/series.py
@@ -1,5 +1,14 @@
 #_*_ coding: utf-8 _*_
 
+
 def fib(a):
     """Return value of ath number in fib sequence."""
-    
+    if a <= 0:
+        return 0
+    elif a == 1:
+        return 0
+    elif a == 2:
+        return 1
+    else:
+        return fib(a-1) + fib(a-2)
+
--- src/test_series.py
+++ src/test_series.py
@@ -3,6 +3,21 @@
 import pytest
 
 
-def test_fib_0():
+def test_fib_1():
     from series import fib
-    assert fib(0) == 1
+    assert fib(1) == 0
+
+
+def test_fib_3():
+    from series import fib
+    assert fib(3) == 1
+
+
+def test_fib_6():
+    from series import fib
+    assert fib(6) == 5
+
+
+def test_fib_20():
+    from series import fib
+    assert fib(20) == 4181
