8061c66ae416225a5d457a752aa7b1360811a07e
ptrompeter <patrick.trompeter@gmail.com>
2016-03-01 15:44:49 -0800

    DONE - sum works, print functions at bottom of series.py file work.

--- src/test_series.py
+++ src/test_series.py
@@ -1,7 +1,5 @@
 #_*_ coding: utf-8 _*_
 
-import pytest
-
 
 def test_fib_0():
     from series import fib
@@ -41,3 +39,13 @@ def test_lucas_5():
 def test_lucas_10():
     from series import lucas
     assert lucas(10) == 123
+
+
+def test_sum_10a():
+    from series import sum
+    assert sum(10) == 55
+
+
+def test_sum_10b():
+    from series import sum
+    assert sum(10, 2, 1) == 123
